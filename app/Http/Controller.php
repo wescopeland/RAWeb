84e5d64b949a26148e1642068526cafe0a1f1265
Shift <shift@laravelshift.com>
2025-04-02 22:40:08 +0000

    Validate via object directly within Controllers

--- app/Http/Controller.php
+++ app/Http/Controller.php
@@ -7,13 +7,11 @@ namespace App\Http;
 use App\Support\Concerns\HandlesResources;
 use App\Support\Concerns\ResolvesSlugs;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests;
-    use ValidatesRequests;
     use HandlesResources;
     use ResolvesSlugs;
 }
